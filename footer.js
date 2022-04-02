8f332e356aee52108dd497e471b26f91125205a1
Santanu55555 <99542723+Santanu55555@users.noreply.github.com>
2022-04-02 18:20:21 +0530

    ok

--- footer.js
+++ footer.js
@@ -1,5 +1,86 @@
 function footer(){
     return `
+    <section class="section">
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-12 col-xs-12">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-12 col-xs-12">
+                            <div class="uppercase">
+                                <span class="footer_heading">ONN Bikes</span>
+                                <ul class="remove_dot">
+                                    <li><a href="/" target="_blank">About Us</a></li>
+                                    <li><a target="_blank" href="/" rel="nofollow">Careers </a><span class="btn-badge">We're hiring!</span>
+                                    </li>
+                                    <li><a>FAQs</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12 col-xs-12">
+                            <div class="uppercase">
+                                <span class="footer_heading"> We're At </span>
+                                <ul class="remove_dot">
+                                    <li>
+                                        <a href="/" target="_self">Bengaluru</a></li>
+                                    <li> <a href="/" target="_self">Hyderabad</a></li>
+                                    <li> <a href="/" target="_self">Jaipur
+                                        </a></li>
+
+
+                                    <li> <a href="/" target="_self">Mysuru</a></li>
+                                    <li> <a href="/" target="_self">Udaipur</a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-12 col-xs-12">
+                            <div class="uppercase">
+                                <span class="footer_heading">Policies</span>
+                                <ul class="remove_dot">
+                                    <li><a title="Our Terms of Use" href="/" >Terms of
+                                            Use</a>
+                                    </li>
+                                    <li><a title="Our Privacy Policy" href="/">Privacy
+                                            Policy</a>
+                                    </li>
+                                    <li><a title="Our Pricing Policy" href="/">Pricing
+                                            Policy</a>
+                                    </li>
+                                    <li><a title="Our Pricing Policy" href="/">Insurance
+                                            Policy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 col-sm-12 col-xs-12">
+                    <div class="uppercase">
+                        <div class="col-md-3">
+                            <a alt="Logo">
+                                <img src="https://d3bvfezcznypk7.cloudfront.net/staticwebsitecontent/img/logo.svg" alt="Logo">
+                            </a>
+                        </div>
+                        <div class="col-md-9 rest-footer-text">
+                            ONN BIKES IS HERE TO REDEFINE THE WAY YOU TRAVEL! <br>
+                            RENT A BIKE ON AN HOURLY, DAILY, WEEKLY OR EVEN MONTHLY BASIS.<br> WE ALSO HAVE LONG
+                            TERM
+                            LEASING SOLUTIONS FOR BUSINESSES<br> WHO ARE INTO LAST MILE DELIVERY THROUGH TWO WHEELERS.
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-12 copyright">
+                    <p> © 2019 motocruizer technologies india pvt.ltd. all rights reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
    `
 }
-export {footer}
+export default footer
